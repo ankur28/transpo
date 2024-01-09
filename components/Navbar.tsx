439cae12fad17a014650bad5c7758e4be8496f6c
ankur28 <ankurkalson28@gmail.com>
2024-01-09 21:28:08 +0530

    basic layout

---
+++ components/Navbar.tsx
@@ -0,0 +1,66 @@
+"use client";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import { UserButton } from "@clerk/nextjs";
+
+function Navbar() {
+  const [darkMode, setDarkMode] = useState(true);
+
+  useEffect(() => {
+    const theme = localStorage.getItem("theme");
+
+    if (theme === "dark") setDarkMode(true);
+
+    console.log("theme mounted", theme);
+  }, []);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.documentElement.classList.remove("light");
+      localStorage.setItem("theme", "light");
+    }
+  }, [darkMode]);
+
+  return (
+    <div
+      className="flex items-center justify-between p-3 px-10 
+    gap-10 shadow-sm border-b-[1px]"
+    >
+      <div className="flex items-center gap-10">
+        <Image
+          src={darkMode ? "/logo-dark.png" : "/logo.png"}
+          alt=""
+          width={120}
+          height={80}
+        />
+
+        <div className=" hidden md:flex gap-6">
+          <h2
+            className="hover:bg-gray-100 hover:dark:bg-gray-700
+           p-2 rounded-md cursor-pointer transition-all"
+          >
+            Home
+          </h2>
+          <h2
+            className="hover:bg-gray-100 hover:dark:bg-gray-700
+           p-2 rounded-md cursor-pointer transition-all"
+          >
+            History
+          </h2>
+          <h2
+            className="hover:bg-gray-100 hover:dark:bg-gray-700
+           p-2 rounded-md cursor-pointer transition-all"
+          >
+            Help
+          </h2>
+        </div>
+      </div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+}
+
+export default Navbar;
